e4ba0bb274ed6957608f21d4b80aee283b4eb0f0
Cleomilson Sales <cleomilsonsales@hotmail.com>
2022-03-28 21:26:41 -0300

    estudos de segunda 28.03.2022 21:26

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //dessa forma o eloquent não segue o padrão de entendimento de nome da tabela pela class
+    protected $table = 'fornecedores'; 
+    //autorizando o metodo estatico create a usar os campos
+    protected $fillable = ['nome','site','uf','email'];
 }
+
+
--- app_super_gestao/app/SiteContato.php
+++ app_super_gestao/app/SiteContato.php
@@ -4,6 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/*
+    Como o eloquent detecta da tabela no laravel
+    -todas as letras maiuscula são consideradas para inserção de um "_"
+    -depois ele deixa todas minusculas e no final acrescenta o S, assim ele acha a tabela
+    -ex.: nessa class a tabela seria site_contatos
+*/
+
 class SiteContato extends Model
 {
     //
